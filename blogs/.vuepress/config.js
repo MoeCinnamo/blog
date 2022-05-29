a910491c7e5ad605cb31dc99c7dff4c3cb4ce85a
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-29 12:17:39 +0800

    Update config.js

--- blogs/.vuepress/config.js
+++ blogs/.vuepress/config.js
@@ -91,6 +91,7 @@ module.exports = {
       appId: '514CxHf6tf8VB7b9e1azmUu8-MdYXbMMI', // your appId
       appKey: 'W0K26NS50iK3hk7GTm4rnLjN', // your appKey
       placeholder: '嘿^_^, 留个评论好不好嘛~',
+      serverURLs: 'https://514cxhf6.api.lncldglobal.com',
       visitor: true,
       recordIP: true,
       enableQQ: true
