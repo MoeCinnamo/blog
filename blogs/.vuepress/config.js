e70de27f48d9a879f5d88d21ceec1f25ea6706e9
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-30 12:31:08 +0800

    Update config.js

--- blogs/.vuepress/config.js
+++ blogs/.vuepress/config.js
@@ -42,6 +42,8 @@ module.exports = {
         ariaLabel: 'contact',
         items: [
           { text: 'GitHub', link: 'https://github.com/MoeCinnamo', icon: 'reco-github' },
+          { text: '今日头条', link: 'https://www.toutiao.com/c/user/token/MS4wLjABAAAAiSeSKmi3utivPvvIzLLM_fHiTyY6whoPfTG5ipt1apg',icon: 'reco-toutiao' },
+          { text: '抖音', link: 'https://www.douyin.com/user/MS4wLjABAAAAVSsT17E5DRZFg4S_FOuEnOs2dSJsWmSoSOWiakPp0zg', icon: 'reco-douyin' },
           { text: 'Email', link: 'mailto:abcd2890000456@126.com',icon: 'reco-message' },
           { text: 'QQ', link: 'http://wpa.qq.com/msgrd?v=3&uin=597624858&site=qq&menu=yes',icon: 'reco-qq' }
         ],
