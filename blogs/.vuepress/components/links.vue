103d7826983bad94300f2bc65bc6b9d25d5756da
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-06-04 20:07:48 +0800

    Update links.vue

--- blogs/.vuepress/components/links.vue
+++ blogs/.vuepress/components/links.vue
@@ -246,6 +246,19 @@
             <small>聞こえるはずの声</small>
           </div>
           
+          <div style="border: 2px solid #d0d0d0;border-radius: 15px;width: 50%;height: 100px;position: relative;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;border: 1px solid #ccc;float: left;box-sizing: border-box;">
+            <a href="http://www.0558.la" target="_blank">
+              <img src="http://www.0558.la/favicon.ico" style="width: 64px;height: 64px;position: absolute;left: 5px;top: 15px;border-radius: 100px">
+            </a>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="http://www.0558.la" target="_blank">
+              <strong>0558自动秒收录</strong>
+            </a>
+            <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <small>0558自动秒收录网</small>
+          </div>
+          
           <br>
           <br><br><br><br><br>
           <br><br><br><br><br>
