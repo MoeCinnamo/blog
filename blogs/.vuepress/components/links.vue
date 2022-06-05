d0d9ab8114ee4efb53efe5fa8959a0c0272c9796
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-06-05 15:16:38 +0800

    Update links.vue

--- blogs/.vuepress/components/links.vue
+++ blogs/.vuepress/components/links.vue
@@ -159,7 +159,7 @@
         
           <div style="border: 2px solid #d0d0d0;border-radius: 15px;width: 50%;height: 100px;position: relative;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;border: 1px solid #ccc;float: left;box-sizing: border-box;">
             <a href="https://blog.iucky.cn" target="_blank">
-              <img src="https://cdn.my-api.cn/user/w/asset-pic-gitee/20220312201745.png" style="width: 64px;height: 64px;position: absolute;left: 5px;top: 15px;border-radius: 100px">
+              <img src="/links/wibus.webp" style="width: 64px;height: 64px;position: absolute;left: 5px;top: 15px;border-radius: 100px">
             </a>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <a href="https://blog.iucky.cn" target="_blank">
@@ -198,7 +198,7 @@
           
           <div style="border: 2px solid #d0d0d0;border-radius: 15px;width: 50%;height: 100px;position: relative;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;border: 1px solid #ccc;float: left;box-sizing: border-box;">
             <a href="https://www.timochan.cn" target="_blank">
-              <img src="https://cdn.timochan.cn/img/avatarcache/avatar.png" style="width: 64px;height: 64px;position: absolute;left: 5px;top: 15px;border-radius: 100px">
+              <img src="/links/Timochan.webp" style="width: 64px;height: 64px;position: absolute;left: 5px;top: 15px;border-radius: 100px">
             </a>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <a href="https://www.timochan.cn" target="_blank">
