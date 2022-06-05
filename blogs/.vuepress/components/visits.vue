12f7fcba3e0dd0e1b025d5de9ea1b63984c63bed
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-06-05 14:16:44 +0800

    Update and rename statistics.vue to visits.vue

---
+++ blogs/.vuepress/components/visits.vue
@@ -0,0 +1,1 @@
+<script id="LA-DATA-WIDGET" crossorigin="anonymous" charset="UTF-8" src="https://v6-widget.51.la/v6/JfZeeuAAChksNoMw/quote.js?theme=0&col=true&f=14&badge=icon_1&icon=center"></script>
